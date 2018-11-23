ef5264ffcd3525392cc8635eacb6c7566f4e0b74
macintario <macintario@gmail.com>
2018-11-22 20:26:49 -0600

    inicial2

--- src/main/scala/introduccionAscala.scala
+++ src/main/scala/introduccionAscala.scala
@@ -1,5 +1,16 @@
 object introduccionAscala {
   def main(args: Array[String]):Unit={
     println("Hello Scala")
+    val tupla = (a: Int) => (a, a+2)
+    println(tupla(1))
+    println(tupla(1)._2)
+    println(tupla(1)._1)
+
+    val y = 10
+    var x= 10
+    x = 15
+
+
+
   }
 }
