e854fe3b3cf7ebfbb3db97b4eab0ffc5056e8569
macintario <macintario@gmail.com>
2018-11-27 20:51:10 -0600

    leidos archivos y fechas como ints

--- src/main/scala/didscala.scala
+++ src/main/scala/didscala.scala
@@ -1,5 +1,11 @@
 import scala.io.Source
 import scala.reflect.ClassTag
+import breeze.linalg.{DenseMatrix, DenseVector, sum}
+import breeze.stats.regression.leastSquares
+
+import scala.reflect.ClassTag
+
+
 
 object didscala {
   def main(args: Array[String]):Unit= {
@@ -8,15 +14,20 @@ object didscala {
   def analiza() = {
     var DataDirectory = "/home/aulae1-b6/map-reduce/holaScala/src/main/resources/"
 
-    val fileName = "distanciaCombustible.csv"
+    val fileName = "aceleracionBruscaData.csv"
 
     val file = Source.fromFile(DataDirectory + fileName)
-    val lines = file.getLines.toVector
+    val lines = file.getLines.toVector.drop(1)
     val splitLines = lines.map { _.split(',') }
 
     def fromList[T:ClassTag](index:Int, converter:(String => T)):DenseVector[T] =
       DenseVector.tabulate(lines.size) { irow => converter(splitLines(irow)(index)) }
     //vehiculo,fecha,aceleraciones
+    val vehiculos = fromList(0, elem => elem.substring(8).toInt)
+    val fecha = fromList(1, elem => elem.substring(3,5).toInt)
+    val aceleraciones = fromList(2,elem =>elem.toDouble )
+    //print(vehiculos)
+
 
     println("OK")
   }
