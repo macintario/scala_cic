516c487a37e0bebacdbda66d478c74613edc272f
macintario <macintario@gmail.com>
2018-11-27 21:53:19 -0600

    ya me cansé

--- src/main/scala/didscala.scala
+++ src/main/scala/didscala.scala
@@ -2,6 +2,7 @@ import scala.io.Source
 import scala.reflect.ClassTag
 import breeze.linalg.{DenseMatrix, DenseVector, sum}
 import breeze.stats.regression.leastSquares
+import breeze.stats._
 
 import scala.reflect.ClassTag
 
@@ -29,8 +30,30 @@ object didscala {
     //print(vehiculos)
     val vectorG = (vehiculos :> 21).toDenseVector //Vehiculos intervenidos del 21 en adelante
     val vectorT = (fecha :> 5).toDenseVector  //Fechas posteriores a mayo
-    println(vectorG)
-    println(vectorT)
+    //println(vectorG)
+    //println(vectorT)
+
+    //T = 0; G = 0
+    val acel_00 = aceleraciones(!vectorT(!vectorG)).toDenseVector
+    //T = 0; G = 1
+    val acel_01 = aceleraciones(!vectorT(vectorG)).toDenseVector
+    //T = 1; G = 0
+    val acel_10 = aceleraciones(vectorT(!vectorG)).toDenseVector
+    //T = 1; G = 1
+    val acel_11 = aceleraciones(vectorT(vectorG)).toDenseVector
+
+
+
+    val bet_0 = mean(acel_00)
+    println(bet_0)
+    val bet_1 = mean(acel_10) - bet_0
+    println(bet_1)
+    val bet_2 = mean(acel_10) - bet_0
+    println(bet_2)
+    val bet_3 = (mean(acel_11) - mean(acel_01))
+        - (mean(acel_10)-mean(acel_00))
+    println(bet_3)
+
     println("OK")
   }
   analiza()
